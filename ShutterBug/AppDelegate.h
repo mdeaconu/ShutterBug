dbbca1846b0a2612fc48059df6f446f18f7e4d67
mdeaconu <mihai.deaconu@icloud.com>
2016-10-03 22:21:46 +0300

    new app

---
+++ ShutterBug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShutterBug
+//
+//  Created by Mihai Deaconu on 03/10/16.
+//  Copyright © 2016 Mihai Deaconu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShutterBug/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShutterBug
+//
+//  Created by Mihai Deaconu on 03/10/16.
+//  Copyright © 2016 Mihai Deaconu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
